ae4c76c3d51c9d5c5ab5a6ae954566b5430a0fd2
Kim <vukimdung264@gmail.com>
2021-11-21 12:50:46 +0100

    TODO: Fix fare calculation service Allow free for 30 mins

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -10,22 +10,31 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        long inTime = ticket.getInTime().getTime();
+        long outTime = ticket.getOutTime().getTime();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
 
-        switch (ticket.getParkingSpot().getParkingType()){
-            case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
-                break;
+        float duration = ((float) (outTime - inTime)) / (3600 * 1000);
+
+        double price = 0d;
+        if (duration > 0.5) {
+            switch (ticket.getParkingSpot().getParkingType()){
+                case CAR: {
+                    price = roundPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                    break;
+                }
+                case BIKE: {
+                    price = roundPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                    break;
+                }
+                default: throw new IllegalArgumentException("Unknown Parking Type");
             }
-            case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
-                break;
-            }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
         }
+        ticket.setPrice(price);
+    }
+
+    private double roundPrice(final double price) {
+        return Math.round(price * 100.0) / 100.0;
     }
 }
